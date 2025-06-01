228142c6121a7a09b889a694f528341de0497af6
Pablo Umanzor A. <info@redlibre.cl>
2025-05-31 20:38:15 -0400

    Create main.tf

---
+++ aws-infra-1/modules/vpc/main.tf
@@ -0,0 +1,18 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.0"
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["172.30.2.0/24", "172.30.3.0/24"]
+  private_subnets = ["172.30.4.0/24", "172.30.5.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
