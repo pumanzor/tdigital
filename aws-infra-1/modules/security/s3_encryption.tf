aa6bee5460e9ca331102a8c7cf94c2e04f7dce61
Pablo Umanzor A. <info@redlibre.cl>
2025-06-01 18:34:29 -0400

    Create s3_encryption.tf

---
+++ aws-infra-1/modules/security/s3_encryption.tf
@@ -0,0 +1,9 @@
+resource "aws_s3_bucket_server_side_encryption_configuration" "secure_encryption" {
+  bucket = aws_s3_bucket.secure_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
