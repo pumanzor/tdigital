f54c71470043eaaa7fcd71011f316c06c8bcb7c1
Pablo Umanzor A. <info@redlibre.cl>
2025-06-01 19:59:23 -0400

    Update variables.tf

--- aws-infra-1/modules/ec2/variables.tf
+++ aws-infra-1/modules/ec2/variables.tf
@@ -7,3 +7,9 @@ variable "security_group_ssh_id" {
   description = "ID del Security Group que permite acceso SSH"
   type        = string
 }
+
+variable "subnet_id" {
+  description = "Subnet ID for the EC2 instance"
+  type        = string
+}
+
