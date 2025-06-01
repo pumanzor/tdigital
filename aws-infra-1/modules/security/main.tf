47f2165212c02f051999a1a52833f4f7a705dbc5
Pablo Umanzor A. <info@redlibre.cl>
2025-06-01 17:58:09 -0400

    Update main.tf

--- aws-infra-1/modules/security/main.tf
+++ aws-infra-1/modules/security/main.tf
@@ -4,6 +4,7 @@ resource "aws_security_group" "default_restricted" {
   vpc_id      = var.vpc_id
 
   ingress = []
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -16,6 +17,37 @@ resource "aws_security_group" "default_restricted" {
   }
 }
 
+
+resource "aws_security_group" "ssh_access" {
+  name        = "ec2-ssh-access"
+  description = "Permite acceso SSH desde IP pública definida"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH desde IP pública permitida"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.allowed_ssh_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2-ssh-access"
+  }
+}
+
+
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}
+
 resource "aws_s3_bucket" "secure_bucket" {
   bucket = "secure-bucket-${random_id.bucket_id.hex}"
   acl    = "private"
@@ -23,10 +55,6 @@ resource "aws_s3_bucket" "secure_bucket" {
   tags = {
     Name = "secure-bucket"
   }
-}
-
-resource "random_id" "bucket_id" {
-  byte_length = 4
 }
 
 resource "aws_s3_bucket_public_access_block" "secure_bucket_block" {
