3eda2f7ff63177bf5a9678d4af3c0656a296d020
Pablo Umanzor A. <info@redlibre.cl>
2025-06-01 18:34:12 -0400

    Delete aws-infra-1/modules/s3/s3_encryption.tf

--- aws-infra-1/modules/s3/s3_encryption.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket_server_side_encryption_configuration" "secure_encryption" {
-  bucket = aws_s3_bucket.secure_bucket.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
