7ab212c71b74766ada419d59179e0f002716cbd2
Pablo Umanzor A. <info@redlibre.cl>
2025-06-01 17:59:01 -0400

    Update variables.tf

--- aws-infra-1/modules/security/variables.tf
+++ aws-infra-1/modules/security/variables.tf
@@ -2,3 +2,8 @@ variable "vpc_id" {
   description = "vpc-079e4527f798f5a88"
   type        = string
 }
+
+variable "allowed_ssh_cidr" {
+  description = "Bloque CIDR permitido para acceso SSH"
+  type        = string
+}
