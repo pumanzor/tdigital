2a26270ca29e54b52c84dd18a48a10ceddd96409
Pablo Umanzor A. <info@redlibre.cl>
2025-05-31 20:39:51 -0400

    Create main.tf

---
+++ aws-infra-1/modules/eks/main.tf
@@ -0,0 +1,22 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "20.4.0"
+
+  cluster_name    = "demo-eks"
+  cluster_version = "1.29"
+
+  subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  enable_irsa     = true
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 2
+      max_size     = 3
+      min_size     = 1
+
+      instance_types = ["t3.medium"]
+    }
+  }
+}
