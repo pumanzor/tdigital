5af6697b5f308d0a6287373c429ebb94bbe27513
Pablo Umanzor A. <info@redlibre.cl>
2025-06-01 20:00:29 -0400

    Update main.tf

--- aws-infra-1/modules/ec2/main.tf
+++ aws-infra-1/modules/ec2/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "bastion" {
   ami           = "ami-0c02fb55956c7d316" 
   instance_type = "t3.micro"
-  subnet_id     = module.vpc.public_subnets[0]
-
+#  subnet_id     = module.vpc.public_subnets[0]
+subnet_id     = var.subnet_id
   key_name      = var.ssh_key_name
 
   vpc_security_group_ids = [
